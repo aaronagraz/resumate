332d1020a348394f134627fa7d1bbdfd9272178c
Salvador Espinosa <espinosasalvador94@outlook.com>
2023-06-19 20:35:53 -0600

    having issues with server

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const db = require("./config/connection");
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// * Do we need an Engine for React?
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
