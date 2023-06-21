a02d705a4a2c0c57ab8bb682578bd7c948fa5386
Aaron Agraz <aaron_agraz@hotmail.com>
2023-06-20 21:37:16 -0600

    Connected openai api

--- server/routes/openaiRoutes.js
+++ server/routes/openaiRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { generateText } = require("../controllers/openaiController");
 const router = express.Router();
 
-router.post("/generateText", generateText);
+router.post("/generateText", (req, res) => {
+  res.status(200).json({
+    success: true,
+  });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
-
 // const { RequiredError } = require("openai/dist/base");
 // const path = require("path");
 // const session = require("express-session");
 // const routes = RequiredError("*");
 
-const app = express();
-app.listen(port, () => console.log(`Server started on port ${port}!`));
-const PORT = process.env.PORT || 3001;
-const db = require("./config/connection");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 
+const app = express();
+app.use("/openai", require("./routes/openaiRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}!`));
+
+// const PORT = process.env.PORT || 3001;
+const db = require("./config/connection");
+app.use(express.json());
 // * Do we need an Engine for React?
 // app.engine("handlebars", hbs.engine);
 // app.set("view engine", "handlebars");
 
-app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "public")));
 
