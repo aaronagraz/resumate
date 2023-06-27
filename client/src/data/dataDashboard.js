d1ff7bd0a424facf6a9dccc87c0d277504053104
TenshiAngelina <angelina.hcortes@gmail.com>
2023-06-26 20:37:50 -0600

    Add placeholder info for Questions page and Navbar component

---
+++ client/src/data/dataDashboard.js
@@ -0,0 +1,59 @@
+export const TestsDone = [
+    {
+      model: 'ResumeGenie',
+      report: 'Resume #1',
+      href: '#',
+    },
+    {
+      model: 'ResumeGenie',
+      report: 'Resume #2',
+      href: '#',
+    },
+    {
+      model: 'MoneyWise',
+      report: 'Report #1',
+      href: '#',
+    },
+    {
+      model: 'AdCraft',
+      report: 'Report #1',
+      href: '#',
+    },
+    {
+      model: 'Dating Booster',
+      report: 'Report #1',
+      href: '#',
+    },
+    {
+      model: 'Dating Booster',
+      report: 'Report #2',
+      href: '#',
+    },
+];
+
+export const testGradient = [
+    {
+      title: "ResumeGenie",
+      buttonGradient: "from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500",
+    },
+    {
+      title: "MoneyWise",
+      buttonGradient: "from-purple-500 to-indigo-500 hover:from-red-500 hover:to-yellow-500",
+    },
+    {
+      title: "Dating Booster",
+      buttonGradient: "from-pink-500 to-yellow-500 hover:from-purple-500 hover:to-indigo-500",
+    },
+    {
+      title: "AdCraft",
+      buttonGradient: "bg-gradient-to-r from-fuchsia-500 to-cyan-500",
+    },
+    {
+      title: "LawAssist",
+      buttonGradient: "from-slate-900 to-slate-700",
+    },
+    {
+      title: "EZ Plan",
+      buttonGradient: "from-amber-500 to-pink-500",
+    },
+];
